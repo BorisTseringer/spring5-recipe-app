98f9d01efc7652ebcd23d409144f4011be974108
taskurim <borystserinher@gmail.com>
2020-03-04 18:04:01 +0100

    created MeasurementUnit and added to Ingredient

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -20,6 +20,9 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    @OneToOne
+    private MeasurementUnit measurementUnit;
+
     public Long getId() {
         return id;
     }
@@ -50,5 +53,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public MeasurementUnit getMeasurementUnit() {
+        return measurementUnit;
+    }
+
+    public void setMeasurementUnit(MeasurementUnit measurementUnit) {
+        this.measurementUnit = measurementUnit;
     }
 }
---
+++ src/main/java/guru/springframework/domain/MeasurementUnit.java
@@ -0,0 +1,28 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class MeasurementUnit {
+
+    @Id
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
